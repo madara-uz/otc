f3148f11595b85f25f7836739069b2c446f32887
originalprofil <upmahmudjon88@gmail.com>
2025-08-19 13:21:33 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+API_ID = 23114126  # Telegram API ID
+API_HASH = "6e032f5ed5705898167e26fb9df513c8"
+BOT_TOKEN = "8329885057:AAGIuo0GZ1FpZMBJXCUdRdONtPmS4VHvfPw"
+
+# Majburiy obuna qilinadigan kanallar yoki guruhlar (username ko‘rinishida)
+REQUIRED_CHANNELS = ["names_vip"]
+
+# Adminlar ro'yxati
+ADMINS = [6322771668]  # Telegram user ID
